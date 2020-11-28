a7d721bae457513b11c996b2c5c5fde9b24b54cc
enock24 <enookimuu@gmail.com>
2020-11-28 14:50:13 +0300

    add three classes to config.py

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
+
 from flask import render_template
 from app import app
+
+
 
 # Views
 @app.route('/')
@@ -8,4 +11,16 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('index.html', title = title)
+
+
+@app.route('/new/<new_id>')
+def movie(new_id):
+
+    '''
+    View new page function that returns the new details page and its data
+    '''
+    title = f'You are viewing {new_id}'
+    return render_template('new.html',title = title)
