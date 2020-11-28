9cfa842c5e3c5b3acb43990e5f8487c57d2fdeab
enock24 <enookimuu@gmail.com>
2020-11-28 18:25:42 +0300

    add function  get _news for getting popular news

--- app/views.py
+++ app/views.py
@@ -3,8 +3,8 @@ from flask import render_template
 from app import app
 
 
+from .request import get_news
 
-# Views
 @app.route('/')
 def index():
 
@@ -12,8 +12,11 @@ def index():
     View root page function that returns the index page and its data
     '''
 
+    # Getting popular movie
+    popular_news = get_news('popular')
+    print(popular_news)
     title = 'Home - Welcome to The best News Review Website Online'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title,popular = popular_news)
 
 
 @app.route('/news/<news_id>')
